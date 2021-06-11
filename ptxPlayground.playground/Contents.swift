982f95797a7dd076c023b8354587c6cf7a40ec1f
Penny8336 <60847071s@gapps.ntnu.edu.tw>
2021-06-11 19:43:16 +0800

    add ptxPlayGround

--- ptxPlayground.playground/Contents.swift
+++ ptxPlayground.playground/Contents.swift
@@ -20,13 +20,61 @@ let base64HmacString = Data(hmac).base64EncodedString()
 let authorization = """
 hmac username="\(appId)", algorithm="hmac-sha256", headers="x-date", signature="\(base64HmacString)"
 """
-let url = URL(string: "https://ptx.transportdata.tw/MOTC/v2/Rail/THSR/Station?$top=30&$format=JSON")!
+let url = URL(string: "https://ptx.transportdata.tw/MOTC/v2/Bus/EstimatedTimeOfArrival/City/Taipei/630?$top=5&$format=JSON")!
 var request = URLRequest(url: url)
 request.setValue(xdate, forHTTPHeaderField: "x-date")
 request.setValue(authorization, forHTTPHeaderField: "Authorization")
 URLSession.shared.dataTask(with: request) { (data, response, error) in
     if let data = data {
         let content = String(data: data, encoding: .utf8) ?? ""
-        print(content)
+        print(type(of: content))
+        struct User: Codable {
+            var StopUID: String
+            var StopID: String
+        }
+        
+
+        
+        let jsonData = content.data(using: .utf8)!
+        let users = try! JSONDecoder().decode([User].self, from: jsonData)
+
+        for user in users {
+            print(user.StopUID)
+        }
+        
+        
     }
 }.resume()
+
+//let json = """
+//{
+//
+//"name": "John Davis",
+//"country": "Peru",
+//"use": "to buy a new collection of clothes to stock her shop before the holidays.",
+//"amount": 150
+//
+//}
+//"""
+//
+//struct Loan: Codable {
+//    var name: String
+//    var country: String
+//    var use: String
+//    var amount: Int
+//}
+//
+//let decoder = JSONDecoder()
+//
+//if let jsonData = json.data(using: .utf8) {
+//
+//    do {
+//        let loan = try decoder.decode(Loan.self, from: jsonData)
+//        print(loan)
+//
+//    } catch {
+//        print(error)
+//    }
+//}
+
+
